8e5d0f8cd2760cb165b6d3ce57ff021c4cce0b64
Seungjoon Hwang <asherolleh@gmail.com>
2022-09-05 11:46:31 +0900

    <FEAT> first custom app migrations

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'houses.apps.HousesConfig'
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
+ 
 TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
--- houses/admin.py
+++ houses/admin.py
@@ -1,3 +1,9 @@
+import imp
 from django.contrib import admin
+from .models import House
 
 # Register your models here.
+
+@admin.register(House)
+class HouseAdmin(admin.ModelAdmin):
+    pass
