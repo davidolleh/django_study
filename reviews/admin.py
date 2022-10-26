0bf26684e56b7981321645cb2f8a16daf9ce1a34
davidolleh <asherolleh@gmail.com>
2022-10-27 02:06:26 +0900

    <FEAT> add room admin page filter

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,12 +1,63 @@
 from django.contrib import admin
 from .models import Review
 
+
+class WordFilter(admin.SimpleListFilter):
+    title = "Filter by words!"
+
+    parameter_name = "word"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("good", "Good"),
+            ("great", "Great"),
+            ("awesome", "Awesome"),
+        ]
+
+    def queryset(self, request, reviews):
+
+        word = self.value()
+        if word:
+            return reviews.filter(payload__contains=word)
+        else:
+            return reviews
+
+
+class SatisfactionFilter(admin.SimpleListFilter):
+
+    title = "Satisfaction"
+
+    parameter_name = "satisfaction"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("good", "Good"),
+            ("bad", "Bad"),
+        ]
+
+    def queryset(self, request, reviews):
+        satisfaction = self.value()
+        if satisfaction == "good":
+            filtered_reviews = reviews.filter(rating__gte=3)
+        elif satisfaction == "bad":
+            filtered_reviews = reviews.filter(rating__lt=3)
+        else:
+            return reviews
+
+        return filtered_reviews.only("rating")
+
+
 @admin.register(Review)
-class Reviewadmin(admin.ModelAdmin):
+class ReviewAdmin(admin.ModelAdmin):
     list_display = (
         "__str__",
         "payload",
     )
     list_filter = (
+        WordFilter,
+        SatisfactionFilter,
         "rating",
+        "user__is_host",
+        "room__category",
+        "room__pet_friendly",
     )
--- rooms/admin.py
+++ rooms/admin.py
@@ -2,8 +2,20 @@ from django.contrib import admin
 from .models import Room, Amenity
 
 
+@admin.action(description="Set all prices to zero")
+def resetPrices(model_admin, request, rooms):
+    for room in rooms.all():
+        room.price = 0
+        room.save()
+
+
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
+
+    actions = (
+        resetPrices,
+    )
+
     list_display = (
         "name",
         "price",
@@ -23,6 +35,12 @@ class RoomAdmin(admin.ModelAdmin):
         "pet_friendly",
         "kind",
         "amenities"
+    )
+
+    search_fields = (
+        "^name",
+        "=price",
+        "owner__username",
     )
 
     readonly_fields = (
