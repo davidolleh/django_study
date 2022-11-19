8acd3a9a42b5dfd07095b6dfddeb341c52d5df45
davidolleh <asherolleh@gmail.com>
2022-11-19 20:43:16 +0900

    <FIX> refactor views by using model view sets

--- categories/views.py
+++ categories/views.py
@@ -3,53 +3,13 @@ from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from .models import Category
 from .serializers import CategorySerializer
 
 
 # Create your views here.
-class Categories(APIView):
+class CategoryViewSet(ModelViewSet):
 
-    def get(self, request):
-        allCategory = Category.objects.all()
-        serialize = CategorySerializer(allCategory, many=True)
-        return Response(serialize.data)
-
-    def post(self, request):
-        serializer = CategorySerializer(data=request.data)
-        if serializer.is_valid():
-            new_category = serializer.save()
-            return Response(
-                CategorySerializer(new_category).data
-            )
-        else:
-            return Response(serializer.errors)
-
-
-class CategoryDetail(APIView):
-
-    def get_object(self, pk):
-        try:
-            return Category.objects.get(pk=pk)
-        except Category.DoesNotExist:
-            raise
-
-    def get(self, request, pk):
-        serializer = CategorySerializer(self.get_object(pk))
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        serializer = CategorySerializer(
-            self.get_object(pk),
-            data=request.data,
-            partial=True
-        )
-        if serializer.is_valid():
-            updated_category = serializer.save()
-            return Response(CategorySerializer(updated_category).data)
-        else:
-            return Response(serializer.errors)
-
-    def delete(self, request, pk):
-        self.get_object(pk).delete()
-        return Response(status=HTTP_204_NO_CONTENT)
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
